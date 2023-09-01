5a1f4c2c48604d79841300bb586b356095e51aac
Aayush Mishra <aayush.luv01@gmail.com>
2023-09-01 22:18:14 +0530

    Server Commit

--- inshorts-server/index.js
+++ inshorts-server/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import Connection from './database/db.js';
+import DefaultData from './default.js';
 
 const app = express();
 
@@ -8,3 +9,5 @@ const PORT = 5000;
 Connection();
 
 app.listen(PORT, () => console.log(`Server has been started successfully on PORT ${PORT}`));
+
+DefaultData();
