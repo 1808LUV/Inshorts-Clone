0e2d01aaad892bae891d1a95550bbf15d5a7611c
Aayush Mishra <aayush.luv01@gmail.com>
2023-09-02 18:59:32 +0530

    Commit

--- inshorts-server/database/db.js
+++ inshorts-server/database/db.js
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose';
 
-const Connection = async (username, password) => {
+const Connection = async (username , password) => {
     const URL = `mongodb://${username}:${password}@ac-hgkftyi-shard-00-00.zrqyrh3.mongodb.net:27017,ac-hgkftyi-shard-00-01.zrqyrh3.mongodb.net:27017,ac-hgkftyi-shard-00-02.zrqyrh3.mongodb.net:27017/INSHORTS-CLONE?ssl=true&replicaSet=atlas-fxfa3h-shard-0&authSource=admin&retryWrites=true&w=majority`;
+    
     try{
        await mongoose.connect(URL, {useNewURLParser : true});
        console.log('Database connected successfully');
--- inshorts-server/index.js
+++ inshorts-server/index.js
@@ -1,23 +1,28 @@
-import express from 'express';
-import Connection from './database/db.js';
-import DefaultData from './default.js';
-import Route from './routes/route.js';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import express from "express";
+import Connection from "./database/db.js";
+import DefaultData from "./default.js";
+import Route from "./routes/route.js";
+import cors from "cors";
+import dotenv from "dotenv";
 
 const app = express();
 dotenv.config();
 
 app.use(cors());
-app.use('/', Route);
+app.use("/", Route);
+
 
 const PORT = 5000;
 
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 
-Connection(username, password);
 
-app.listen(PORT, () => console.log(`Server has been started successfully on PORT ${PORT}`));
+
+Connection(username , password);
+
+app.listen(PORT, () =>
+  console.log(`Server has been started successfully on PORT ${PORT}`)
+);
 
 DefaultData();
